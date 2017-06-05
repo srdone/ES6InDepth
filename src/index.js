9b9df6ea6cdeb8d04a44926a03f552344905a9e8
Stephen Done <sdone@simplifile.com>
2017-06-05 09:54:47 -0600

    proxy without traps;

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello world!');
+var target = {};
+var handler = {};
+var proxy = new Proxy(target, handler);
+proxy.a = 'b';
+
+console.log(target.a);
+
+console.log(proxy.c === undefined);
